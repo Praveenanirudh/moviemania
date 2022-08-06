436c54be4a85e1f453ba7396b93c4f5b8c4f1ee9
Praveenanirudh <praveenanirudh998@gmail.com>
2022-08-06 10:31:13 +0530

    Added Navbar and Side bar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Menu,
+  AccountCircle,
+  DarkMode,
+  Brightness7,
+  CallMissedSharp,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+import { Sidebar } from "../index";
+import useStyles from "./styles";
+const NavBar = () => {
+  const [mobileOpen, setmobileOpen] = useState(false);
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const theme = useTheme();
+
+  const isAuthenticated = true;
+  return (
+    <>
+      <AppBar position="fixed" sx={{ backgroundColor: "#010048" }}>
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: "none" }}
+              onClick={() => setmobileOpen((prevMobileOpen) => !prevMobileOpen)}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+            {theme.palette.mode == "dark" ? <Brightness7 /> : <DarkMode />}
+          </IconButton>
+          {!isMobile && "search..."}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to={`/profile/:id`}
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="profile"
+                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && "search..."}
+        </Toolbar>
+      </AppBar>
+      <div>
+        <nav className={classes.drawer}>
+          {isMobile ? (
+            <Drawer
+              variant="temporary"
+              anchor="right"
+              open={mobileOpen}
+              onClose={() => setmobileOpen((prevMobileOpen) => !prevMobileOpen)}
+              classes={{ paper: classes.drawerPaper }}
+              ModalProps={{ keepMounted: true }}
+            >
+              <Sidebar setmobileOpen={setmobileOpen} />
+            </Drawer>
+          ) : (
+            <Drawer
+              classes={{ paper: classes.drawerPaper }}
+              variant="permanent"
+              open
+            >
+              <Sidebar setmobileOpen={setmobileOpen} />
+            </Drawer>
+          )}
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect } from "react";
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+  ListItemIcon,
+  Box,
+  CircularProgress,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+import useStyles from "./style";
+
+const categories = [
+  { label: "Popular", value: "popular" },
+  { label: "Top Rated", value: "top_rated" },
+  { label: "Upcoming", value: "upcoming" },
+];
+const demoCategories = [
+  { label: "Comedy", value: "comedy" },
+  { label: "Action", value: "action" },
+  { label: "Horror", value: "horror" },
+  { label: "Animation", value: "animation" },
+];
+const redLogo =
+  "https://fontmeme.com/permalink/220805/a04e43814aca497eb4f04f6b64121cf3.png";
+const blueLogo =
+  "https://fontmeme.com/permalink/220805/0484e2a51aba1b94e069c26905006d6a.png";
+const Sidebar = (setmobileOpen) => {
+  const theme = useTheme();
+  const classes = useStyles();
+  return (
+    <>
+      <Link to="/" className={classes.imageLink}>
+        <img
+          className={classes.image}
+          src={theme.palette.mode == "light" ? blueLogo : redLogo}
+          alt="MovieMania Logo"
+        />
+      </Link>
+      <Divider />
+      <List>
+        <ListSubheader>Categories</ListSubheader>
+        {categories.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/">
+            <ListItem onClick={() => {}} button>
+              {/* <ListItemIcon>
+                <img
+                  src={redLogo}
+                  className={classes.genreImages}
+                  height={30}
+                />
+              </ListItemIcon> */}
+              <ListItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        <ListSubheader>Genres</ListSubheader>
+        {demoCategories.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/">
+            <ListItem onClick={() => {}} button>
+              {/* <ListItemIcon>
+                <img
+                  src={redLogo}
+                  className={classes.genreImages}
+                  height={30}
+                />
+              </ListItemIcon> */}
+              <ListItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </>
+  );
+};
+
+export default Sidebar;
